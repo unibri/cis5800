d7d31d8f3124aeb87662beeac7454ca71cf60900
Briant Illescas <BRIANT.ILLESCAS@baruchmail.cuny.edu>
2019-12-09 23:21:48 -0500

    -added foooter -added google translate API

--- results.php
+++ results.php
@@ -415,7 +415,16 @@
 		</div>
 	</div>
 </div>
+<footer>
+	<div id="google_translate_element"></div>
+</footer>
     <!-- Optional JavaScript -->
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
